f888bbc8f0863483813458ff3486fdd5190ef129
Adriano <adriano.flip1273@gmail.com>
2023-01-16 02:23:14 -0300

    controllers setup

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # README
 
-* Ruby version
-    Ruby 3.0.0
-    Using rails 7.0.4
+## Project info:
+- Ruby 3.0.0
+- Using rails 7.0.4
+
+## Para rodar o projeto digite no terminal:
+    $ bundle install
+    $ rails db:drop db:create db:migrate db:seed
+
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :pokemons
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '/pokemons' do
+
+    get '/', to: 'pokemons#index'
+    get '/:id', to: 'pokemons#show'
+
+  end
+  
 end
